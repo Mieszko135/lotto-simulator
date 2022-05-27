0301206728b8402cbb2b702436097bb5f9dc7b93
Mieszko135 <mieszko-braun@wp.pl>
2022-05-27 13:57:14 +0200

    draw balls algorithm

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const choosenNumbers = [];
 const drawnNumbers = [];
 
 const couponNumbers = document.querySelectorAll("td");
+const lottoBalls = document.querySelectorAll(".ball");
 
 couponNumbers.forEach((number) => {
     const markedNumber = parseInt(number.textContent);
@@ -31,6 +32,42 @@ couponNumbers.forEach((number) => {
         })
     }
 })
+
+// for (let i = 0; i < 6; i++) {
+//     let lotteryNumber = randomNumber();
+//     if (!drawnNumbers.includes(lotteryNumber)) {
+//         drawnNumbers.push(lotteryNumber)
+//     } else {
+//         i -= 1;
+//     }
+// }
+
+let counter = 0;
+const lottoInterval = setInterval(() => {
+    let lotteryNumber = randomNumber();
+    if (!drawnNumbers.includes(lotteryNumber)) {
+         drawnNumbers.push(lotteryNumber)
+    } else {
+        counter -= 1;
+    }
+    lottoBalls.forEach((ball, index) => {
+        ball.textContent = drawnNumbers[index];
+    });
+    counter++;
+    if (counter === 6) {
+        clearInterval(lottoInterval)
+    }
+}, 2000);
+
+// lottoBalls.forEach((ball, index) => {
+//     ball.textContent = drawnNumbers[index];
+// })
+
+
+
+// setTimeout(() => {
+//     clearInterval(lottoInterval)
+// }, 18000);
 
 
 // couponNumbers.forEach((number) => {
